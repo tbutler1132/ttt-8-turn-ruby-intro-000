69a3e53634f0284c1c5fadbe92ca3dba7711b8b6
artificial-composition-7121 <tbutler1132@gmail.com>
2020-06-16 20:34:17 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,6 +40,9 @@ def turn(board)
   input = gets.strip
   input_to_index(input)
   if valid_move?(board, index)
-    true
+    move(array, index, value = "X")
+    display_board(board)
+  else
+    puts "Please enter 1-9:"
   end
 end
