e2739fe7e9d73c69c41ca77da57ee95dcb3c2063
artificial-composition-7121 <tbutler1132@gmail.com>
2020-06-16 21:20:48 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,4 @@
-
-
-#Display a tic tac toe board
+#displays a tic tac toe board, with board spaces passed as an array
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -9,36 +7,53 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-#convert user input to index by subtracting 1
+#coverts a user's place on the board to the index integer
 def input_to_index(user_input)
-"#{user_input}".to_i - 1
+  index = user_input.to_i
+  index -= 1
+  return index
 end
 
-#checks the user_input to see if that position is taken and it is a valid number
-def position_taken?(board, index)
-    if board[index] == ' ' || board[index] == '' || board[index] == nil
-        false
-      else
-        true
-      end
-    end
-
+#checks the number to see if 1) the position is already taken and 2)it is an actual number
 def valid_move?(board, index)
-  if !position_taken?(board, index) && (index).between?(0,8)
-    true
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
+  end
+
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
   else
-    false
+    return false
   end
 end
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-def move(array, index, value = "X")
-  array[index] = value
+#Takes a users num and the board, and places the X or O char in that position
+def move(board, index, character = "X")
+  board[index] = character
+  return board
 end
 
-def turn(board)
+#Asks user for a number, check if it is valid, and if it's not, recursively continues to ask for a number
+def turn (board)
   puts "Please enter 1-9:"
-  user_input = gets.strip
-  user_input = input_to_index(user_input)
+  num = gets.chomp
+  index = input_to_index(num)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
