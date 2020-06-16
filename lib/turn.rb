102796bcb5fc21b98e9c459368ec0c670241230f
artificial-composition-7121 <tbutler1132@gmail.com>
2020-06-16 20:43:37 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,7 +40,7 @@ def turn(board)
   input = gets.strip
   input_to_index(input)
   if valid_move?(board, index)
-    move (array, index, value)
+    move(array, index, value)
   else
     def turn(board)
 end
